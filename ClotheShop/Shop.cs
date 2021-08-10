0812372b124f77196ec58b804e8a727da79e3936
Pelli <friedstudios@outlook.com>
2021-08-10 16:12:43 -0300

    Class Shop Created

---
+++ ClotheShop/Shop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClotheShop
+{
+    class Shop
+    {
+        private String name;
+        private String direction;
+        private List<Garment> listGarments;
+
+        private Shop(String name, String direction)
+        {
+            this.name = name;
+            this.direction = direction;
+            listGarments = new List<Garment>();
+        }
+    }
+}
