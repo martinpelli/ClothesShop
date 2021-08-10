b102d2e2e2b3f488d04dc8f29777c32eb845bbd5
Pelli <friedstudios@outlook.com>
2021-08-10 16:06:12 -0300

    Class Garment created

---
+++ ClotheShop/Garment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClotheShop
+{
+    abstract class Garment
+    {
+        protected bool quality;
+        protected float price;
+        protected int stock;
+
+        protected Garment() { }
+        protected Garment(float price, int stock, bool quality = false)
+        {
+            this.quality = quality;
+            this.price = price;
+            this.stock = stock;
+        }
+
+        protected abstract float CheckPrice();
+
+    }
+}
+
