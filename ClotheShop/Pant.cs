f6d9a8d7335d86ede8711f1079fdad0777edade1
Pelli <friedstudios@outlook.com>
2021-08-10 16:07:51 -0300

    Class Pant Created

---
+++ ClotheShop/Pant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClotheShop
+{
+    class Pant: Garment
+    {
+        private bool pantType;
+
+        private Pant(bool pantType = false)
+        {
+            this.pantType = pantType;
+        }
+
+        protected override float CheckPrice()
+        {
+            return (0);
+        }
+    }
+}
