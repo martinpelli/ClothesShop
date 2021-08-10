bdc652fff8c289a265ed016e11e07cedb090c30a
Pelli <friedstudios@outlook.com>
2021-08-10 16:09:37 -0300

    Class Shirt created

---
+++ ClotheShop/Shirt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClotheShop
+{
+    class Shirt : Garment
+    {
+        private bool sleeveType;
+        private bool collarType;
+
+        private Shirt(bool sleeveType = false, bool collarType = false)
+        {
+            this.sleeveType = sleeveType;
+            this.collarType = collarType;
+        }
+        protected override float CheckPrice()
+        {
+            return (0);
+        }
+    }
+}
