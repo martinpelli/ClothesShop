402e63eb6f79c9465b3578f978f5fb9e3533b6eb
Pelli <friedstudios@outlook.com>
2021-08-10 16:04:23 -0300

    Class Seller Created

---
+++ ClotheShop/Seller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClotheShop
+{
+    class Seller
+    {
+        private String name;
+        private String lastName;
+        private int codS;
+
+        private Seller(String name, String lastName, int codS)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.codS = codS;
+        }
+    }
+}
